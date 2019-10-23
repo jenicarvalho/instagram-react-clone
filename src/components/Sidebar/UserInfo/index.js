13616f84411553ff33cd8aaec62dae04d6c8e97c
Jeniffer Carvalho <jenicarvalho@live.com>
2019-10-22 22:16:33 -0300

    adicionado consumer a sidebar

--- src/components/Sidebar/UserInfo/index.js
+++ src/components/Sidebar/UserInfo/index.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { UserContext } from "../../../context/userProvider";
 
 import { Container, User } from "./styles";
 
 export default function UserInfo() {
   return (
-    <Container>
-      <img
-        alt="Perfil"
-        src="https://randomuser.me/api/portraits/women/25.jpg"
-      />
-      <User>
-        <strong>jenifcarvalho</strong>
-        <span>Jeniffer Carvalho</span>
-      </User>
-    </Container>
+    <UserContext.Consumer>
+      {
+        state => (
+        <Container>
+          <img
+            alt="Perfil"
+            src="https://randomuser.me/api/portraits/women/25.jpg"
+            />
+          <User>
+            <strong>{state.user}</strong>
+            <span>{state.name}</span>
+          </User>
+        </Container>
+      )
+    }
+    </UserContext.Consumer>
   );
 }
