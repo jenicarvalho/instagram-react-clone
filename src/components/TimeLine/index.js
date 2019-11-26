db7d3ab1d1d8522a8a27f5baa4899fda769590aa
Jeniffer Carvalho <jenicarvalho@live.com>
2019-11-25 21:27:24 -0300

    request usando useEffect e loop atraves dos posts

--- src/components/TimeLine/index.js
+++ src/components/TimeLine/index.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Post from "./Post";
 import { Container } from "./styles";
 
 export default function TimeLine() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/posts", {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(response => {
+        setPosts(response);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <Container>
-      <Post />
-      <Post />
+      {posts.map(post => (
+        <Post key={post.id} content={post} />
+      ))}
     </Container>
   );
 }
