5a9b0989342757d9fa0daf163c6988163e8bbdcc
Jeniffer Carvalho <jenicarvalho@live.com>
2019-11-25 21:27:55 -0300

    adicionado conteudo dos posts via props

--- src/components/TimeLine/Post/index.js
+++ src/components/TimeLine/Post/index.js
@@ -2,22 +2,22 @@ import React from "react";
 
 import { Container, User, PostImage } from "./styles";
 
-export default function Post() {
+export default function Post(props) {
   return (
     <Container>
       <header>
-        <img src="https://randomuser.me/api/portraits/men/12.jpg" alt="user" />
+        <img src={props.content.userPicture} alt="user" />
         <User>
-          <strong>Max Goulart</strong>
-          <span>Château de Vianden</span>
+          <strong>{props.content.user}</strong>
+          {props.content.location !== undefined && (
+            <span>{props.content.location}</span>
+          )}
         </User>
       </header>
       <PostImage>
-        <img
-          src="https://instagram.fcpq4-1.fna.fbcdn.net/vp/001f8d8336d7f0848ea869f5b8b9af6a/5E374FE7/t51.2885-15/sh0.08/e35/s640x640/69882750_130088278326589_2566948334102309039_n.jpg?_nc_ht=instagram.fcpq4-1.fna.fbcdn.net&_nc_cat=1"
-          alt="post"
-        />
+        <img src={props.content.postPicture} alt="post" />
       </PostImage>
+      <p>{props.content.description}</p>
     </Container>
   );
 }
