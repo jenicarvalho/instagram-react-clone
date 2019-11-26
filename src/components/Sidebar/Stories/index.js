1d16b779c4cf65ac519e27e9cd636e553ae268da
Jeniffer Carvalho <jenicarvalho@live.com>
2019-11-26 20:28:08 -0300

    adicionado hooks e feito loop pelas stories

--- src/components/Sidebar/Stories/index.js
+++ src/components/Sidebar/Stories/index.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Storie from "./Storie";
 import { Container } from "./styles";
 
 export default function Stories() {
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/stories", {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(response => {
+        setStories(response);
+      })
+      .catch(error => console.log(error));
+  }, []);
   return (
     <Container>
       <h2>Stories</h2>
 
-      <Storie />
-      <Storie />
-      <Storie />
+      {stories.map(storie => (
+        <Storie key={storie.id} content={storie} />
+      ))}
     </Container>
   );
 }
